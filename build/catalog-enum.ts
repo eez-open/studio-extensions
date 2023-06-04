fd4da21520518d258d45c5308d55ef8d1d26bd19
Martin Vladic <martin.vladic@gmail.com>
2023-06-04 18:28:45 +0200

    pext support

--- build/catalog-enum.ts
+++ build/catalog-enum.ts
@@ -1,5 +1,5 @@
 import { readFile, writeFile } from "fs";
-var request = require('request-promise-native');
+var request = require("request-promise-native");
 var sha256 = require("sha256");
 import * as decompress from "decompress";
 import * as Jimp from "jimp";
@@ -10,9 +10,12 @@ async function getExtensionsList() {
             if (err) {
                 reject(err);
             } else {
-                resolve(data.split("\n").map(line => line.trim()).filter(line => 
-                    line !== '' && !line.startsWith('#')
-                ));
+                resolve(
+                    data
+                        .split("\n")
+                        .map((line) => line.trim())
+                        .filter((line) => line !== "" && !line.startsWith("#"))
+                );
             }
         });
     });
@@ -37,75 +40,121 @@ async function getRepositoryCatalogs() {
     const repositoryCatalogs = [];
 
     for (const extension of extensions) {
-        let downloadUrl: string;
-        let extensionZipFileData;
-        if (extension.startsWith('.')) {
-            // local
-            downloadUrl = "https://github.com/eez-open/studio-extensions/raw/master" + extension.substr(1);
-            extensionZipFileData = await new Promise<string>((resolve, reject) => {
-                readFile(__dirname + "/../" + extension, "binary", (err, data) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(data);
-                    }
-                });
+        if (extension.startsWith("pext:")) {
+            const name = extension.substring("pext:".length);
+
+            const url = `https://registry.npmjs.org/${name}`;
+
+            const jsonStr = await request({
+                method: "GET",
+                url,
+                encoding: "utf-8",
+            });
+
+            const json = JSON.parse(jsonStr)
+
+            repositoryCatalogs.push({
+                name,
+                description: json.versions[json["dist-tags"].latest].description,
+                "eez-studio": {
+                    minVersion: "0.10.1",
+                },
             });
         } else {
-            // remote
-            downloadUrl = extension;
-            extensionZipFileData = await request({
-                method: 'GET',
-                url: downloadUrl,
-                encoding: null
-            });
+            let downloadUrl: string;
+            let extensionZipFileData;
+            if (extension.startsWith(".")) {
+                // local
+                downloadUrl =
+                    "https://github.com/eez-open/studio-extensions/raw/master" +
+                    extension.substr(1);
+                extensionZipFileData = await new Promise<string>(
+                    (resolve, reject) => {
+                        readFile(
+                            __dirname + "/../" + extension,
+                            "binary",
+                            (err, data) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve(data);
+                                }
+                            }
+                        );
+                    }
+                );
+            } else {
+                // remote
+                downloadUrl = extension;
+                extensionZipFileData = await request({
+                    method: "GET",
+                    url: downloadUrl,
+                    encoding: null,
+                });
+            }
+
+            const files = await decompress(
+                Buffer.from(extensionZipFileData, "binary")
+            );
+
+            const packageJsonFile = files.find(
+                (file) => file.path === "package.json"
+            );
+            if (!packageJsonFile) {
+                return undefined;
+            }
+
+            const packageJson = JSON.parse(packageJsonFile.data.toString());
+
+            if (!packageJson["eez-studio"]) {
+                return undefined;
+            }
+
+            const imagePath = packageJson.image || "image.png";
+            const imageFile = files.find((file) => file.path === imagePath);
+            if (imageFile) {
+                const image = await Jimp.read(imageFile.data);
+                packageJson.image = await image
+                    .resize(256, (image.getHeight() * 256) / image.getWidth())
+                    .getBase64Async(image.getMIME());
+            }
+
+            packageJson.download = downloadUrl;
+
+            let extensionHash = extensionHashes.find(
+                (extensionHash: any) =>
+                    extensionHash.downloadUrl === downloadUrl
+            );
+            if (!extensionHash) {
+                console.log(
+                    "New SHA256 hash generated for download url: ",
+                    downloadUrl
+                );
+                extensionHash = {
+                    downloadUrl,
+                    sha256: sha256(extensionZipFileData),
+                };
+                extensionHashes.push(extensionHash);
+            }
+            packageJson.sha256 = extensionHash.sha256;
+
+            repositoryCatalogs.push(packageJson);
         }
-
-        const files = await decompress(Buffer.from(extensionZipFileData, "binary"));
-
-        const packageJsonFile = files.find(file => file.path === "package.json");
-        if (!packageJsonFile) {
-            return undefined;
-        }
-
-        const packageJson = JSON.parse(packageJsonFile.data.toString());
-
-        if (!packageJson["eez-studio"]) {
-            return undefined;
-        }
-
-        const imagePath = packageJson.image || "image.png";
-        const imageFile = files.find(file => file.path === imagePath);
-        if (imageFile) {
-            const image = (await Jimp.read(imageFile.data));
-            packageJson.image = await image.resize(256, image.getHeight() * 256 / image.getWidth()).getBase64Async(image.getMIME());
-        }
-
-        packageJson.download =
-            downloadUrl;
-
-        let extensionHash = extensionHashes.find((extensionHash: any) => extensionHash.downloadUrl === downloadUrl);
-        if (!extensionHash) {
-            console.log("New SHA256 hash generated for download url: ", downloadUrl);
-            extensionHash = {
-                downloadUrl,
-                sha256: sha256(extensionZipFileData)
-            };
-            extensionHashes.push(extensionHash);
-        }
-        packageJson.sha256 = extensionHash.sha256;
-
-        repositoryCatalogs.push(packageJson);
     }
 
     await new Promise<void>((resolve, reject) => {
-        writeFile(__dirname + "/extension-hashes.json", JSON.stringify(extensionHashes, undefined, 4), "utf8", err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
+        writeFile(
+            __dirname + "/extension-hashes.json",
+            JSON.stringify(extensionHashes, undefined, 4),
+            "utf8",
+            (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
             }
-        });
+        );
     });
 
     return repositoryCatalogs;
